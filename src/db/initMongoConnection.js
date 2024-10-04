dc534f1e28f51dfce4eacf052b5e82596003c989
NinaKulyk <nina1998lukashova@gmail.com>
2024-10-05 00:10:13 +0100

    finish

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
+import { MONGO_DB_VARS } from '../constants/index.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = env(MONGO_DB_VARS.MONGODB_USER);
+    const password = env(MONGO_DB_VARS.MONGODB_PASSWORD);
+    const url = env(MONGO_DB_VARS.MONGODB_URL);
+    const db = env(MONGO_DB_VARS.MONGODB_DB);
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+    );
+
+    console.log('Mongo connection successfully established!');
+  } catch (err) {
+    console.log('Error connecting to MongoDB:', err);
+    throw err;
+  }
+};
